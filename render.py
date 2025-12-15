2ad30f39e0a1fa7646aa207929956d9d944ce896
Thomas Nelson <tanelso2@gmail.com>
2025-12-15 12:39:50 -0600

    Refactor to do more preprocessing in Python

--- render.py
+++ render.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python
 
+from dataclasses import dataclass
 import json
 from pprint import pprint
 from typing import Any, Callable, Iterable, Optional
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from markdown_it import MarkdownIt
+
+
+COLUMN_ORDER = [
+    'Player',
+    'MA', 
+    'ST', 
+    'AG', 
+    'AV',  
+    'Skills & Traits', 
+    'Primary', 
+    'Secondary', 
+    'Cost', 
+    'SPP', 
+    'Keywords'
+]
 
 
 def render(**context) -> str:
@@ -16,7 +32,7 @@ def render(**context) -> str:
     md = MarkdownIt()
     env.filters['md'] = md.render
     template = env.get_template('bb_cards.html.j2')
-    return template.render(**context)
+    return template.render(COLUMN_ORDER=COLUMN_ORDER, **context)
 
 
 def get_selections(data: dict) -> list[dict]:
@@ -65,6 +81,114 @@ def uniq_by(xs: Iterable[Any], transform: Callable | str) -> list:
     return ret
 
 
+def dedupe_by(xs: Iterable[Any], transform: Callable | str) -> list:
+    groups = group_by(xs, transform)
+    ret = []
+    for k in sorted(groups.keys()):
+        vals = groups.get(k)
+        count = len(vals)
+        entry = vals[0]
+        entry['count'] = count
+        ret.append(entry)
+    return ret
+
+
+def characteristics_dict(characteristics: list[dict]) -> dict:
+    return { c['name']: c['$text'] for c in characteristics }
+
+
+def cost_dict(costs: list[dict]) -> dict:
+    return { c['name']: c['value'] for c in costs }
+
+
+@dataclass
+class Profile:
+    id: str
+    name: str
+    characteristics: dict
+
+    @staticmethod
+    def parse(data: dict):
+        chars = characteristics_dict(data.get('characteristics', []))
+        return Profile(
+            id=data['id'],
+            name=data['name'],
+            characteristics=chars
+        )
+
+
+def get_profiles(data: dict) -> list[Profile]:
+    all_profiles = []
+    for s in get_selections(data):
+        all_profiles.extend(s.get('profiles', []))
+    
+    profiles = dedupe_by(all_profiles, 'id')
+    profiles = [Profile.parse(p) for p in profiles]
+    profiles.sort(key=lambda x: x.name)
+    return profiles
+
+
+def merge_costs(costs_list: list[dict]) -> dict:
+    merged = {}
+    for costs in costs_list:
+        for name, value in costs.items():
+            if name in merged:
+                merged[name] += value
+            else:
+                merged[name] = value
+    return merged
+
+
+@dataclass
+class Player:
+    name: str
+    profiles: list[Profile]
+    costs: dict
+    primary_category: Optional[str]
+    category_names: list[str]
+    custom_name: Optional[str]
+    selections: list[dict]
+    rules: list[dict]
+    characteristics: dict
+
+    @staticmethod
+    def parse(data: dict):
+        profiles = [Profile.parse(p) for p in data.get('profiles', [])]
+        primary_profile = profiles[0] if profiles else None
+        costs = cost_dict(data.get('costs', []))
+        primary_cat = primary_category(data)
+        categories = data.get('categories', [])
+        category_names = [c['name'] for c in categories]
+        selections = data.get('selections', [])
+        all_costs = [costs]
+        all_rules = []
+        all_rules.extend(data.get('rules', []))
+        for s in selections:
+            if 'costs' in s:
+                all_costs.append(cost_dict(s['costs']))
+            all_rules.extend(s.get('rules', []))
+        total_costs = merge_costs(all_costs)
+        all_rules = uniq_by(all_rules, 'name')
+        characteristics = primary_profile.characteristics if primary_profile else {}
+        if 'TV' in total_costs:
+            # Add commas to number
+            characteristics['Cost'] = f"{total_costs['TV']:,}"
+        if 'SPP' in total_costs:
+            characteristics['SPP'] = str(total_costs['SPP'])
+        characteristics['Player'] = data['name']
+        return Player(
+            name=data['name'],
+            profiles=profiles,
+            costs=total_costs,
+            primary_category=primary_cat,
+            category_names=category_names,
+            custom_name=data.get('customName'),
+            selections=selections,
+            rules=all_rules,
+            characteristics=characteristics
+        )
+
+
 def get_rules(data: dict) -> list[dict]:
     rules = []
     for s in get_selections(data):
@@ -72,10 +196,12 @@ def get_rules(data: dict) -> list[dict]:
     return uniq_by(rules, 'name')
 
 
-def get_players(data: dict) -> list[dict]:
+def get_players(data: dict) -> list[Player]:
     selections = get_selections(data)
     groups = group_by(selections, primary_category)
-    return groups.get('Player', [])
+    players_data = groups.get('Player', [])
+    players = [Player.parse(p) for p in players_data]
+    return players
 
 
 def team_management_options(data: dict):
@@ -102,6 +228,7 @@ def team_management_options(data: dict):
 def render_team(data: dict, include_css=True) -> str:
     rules = get_rules(data)
     players = get_players(data)
+    profiles = get_profiles(data)
     name = data['roster']['name']
     tv = data['roster']['costs'][0]['value']
     league, special_rules, options = team_management_options(data)
@@ -111,6 +238,7 @@ def render_team(data: dict, include_css=True) -> str:
                   special_rules=special_rules,
                   rules=rules, 
                   players=players, 
+                  profiles=profiles,
                   team_management_options=options, 
                   include_css=include_css)
 
